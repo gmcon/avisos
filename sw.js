7d53e6e4eafa23123dca81e4e9cf60e5713bfba0
gmcon <gmcon@yahoo.com>
2025-03-22 21:04:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,10 +15,20 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('avisos-cache-v2').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './avisos.js',
+        './avisos.json',
+        './manifest.json',
+        './icon.png',
+        './icon512.png'
+      ]);
     })
   );
 });
+
